28e80a2186ff222d18c509b78a98fb528ca8a657
Sbarrachina <sandrabarrachina1978@gmail.com>
2023-05-03 14:45:13 +0200

    agregando pagina escritoria slider funcionar con datos json y modo responsivo, fechas y boton cerrar incluidos

--- servicios-2.js
+++ servicios-2.js
@@ -16,3 +16,60 @@ function menutoggle(){
 
     }
 }
+
+
+// datos libros
+
+function cerrarDetalle() {
+    document.querySelector("#detalle-libro").style.display = "none";
+  }
+
+  
+  let libros = [];
+let indiceLibroActual = -1;
+
+fetch("/books.json")
+  .then(response => response.json())
+  .then(data => {
+    libros = data.libros;
+
+    // Agregar evento de click a cada imagen del slider
+    libros.forEach((libro, indice) => {
+      const imagen = document.getElementById(libro.id);
+      imagen.addEventListener("click", () => {
+        indiceLibroActual = indice;
+        mostrarLibroActual();
+        document.getElementById("detalle-libro").style.display = "block";
+      });
+    });
+
+    mostrarLibroActual();
+  });
+
+function mostrarLibroActual() {
+  const libroActual = libros[indiceLibroActual];
+  document.getElementById("titulo").textContent = libroActual.titulo;
+  document.getElementById("descripcion").textContent = libroActual.descripcion;
+  document.getElementById("imagen").src = libroActual.imagen;
+  document.getElementById("anio").textContent = libroActual.anio;
+}
+
+function cerrarDetalle() {
+  document.getElementById("detalle-libro").style.display = "none";
+}
+
+function anteriorLibro() {
+  indiceLibroActual--;
+  if (indiceLibroActual < 0) {
+    indiceLibroActual = libros.length - 1;
+  }
+  mostrarLibroActual();
+}
+
+function siguienteLibro() {
+  indiceLibroActual++;
+  if (indiceLibroActual >= libros.length) {
+    indiceLibroActual = 0;
+  }
+  mostrarLibroActual();
+}
